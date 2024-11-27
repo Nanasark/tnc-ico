42c39265087c15af5ed520c3b0555488cecbf77f
Nanasark <danielnyamekyesarkodie@gmail.com>
2024-11-27 12:19:39 +0000

    accept

--- src/app/abi.ts
+++ src/app/abi.ts
@@ -1,22 +1,13 @@
 export const ICOABI = [
   {
     inputs: [
-<<<<<<< HEAD
       { internalType: "address", name: "_tokenAddress", type: "address" },
-=======
-      {
-        internalType: "contract IERC20",
-        name: "_token",
-        type: "address",
-      },
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
     ],
     stateMutability: "nonpayable",
     type: "constructor",
   },
   {
     inputs: [
-<<<<<<< HEAD
       { internalType: "uint256", name: "_tokenAmount", type: "uint256" },
     ],
     name: "buyToken",
@@ -48,41 +39,12 @@ export const ICOABI = [
       { internalType: "uint256", name: "supply", type: "uint256" },
       { internalType: "uint256", name: "toknePrice", type: "uint256" },
       { internalType: "address", name: "tokenAddr", type: "address" },
-=======
-      {
-        internalType: "address",
-        name: "newWallet",
-        type: "address",
-      },
-    ],
-    name: "addBackendWallet",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    name: "backendWallets",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
     ],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
-<<<<<<< HEAD
     name: "owner",
     outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
@@ -106,102 +68,30 @@ export const ICOABI = [
     inputs: [],
     name: "tokenSalePrice",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-=======
-    name: "getContractTokenBalance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "outToken",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-<<<<<<< HEAD
       { internalType: "address", name: "_tokenAddress", type: "address" },
     ],
     name: "updateToken",
-=======
-      {
-        internalType: "address",
-        name: "wallet",
-        type: "address",
-      },
-    ],
-    name: "removeBackendWallet",
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-<<<<<<< HEAD
       { internalType: "uint256", name: "_tokenSalePrice", type: "uint256" },
     ],
     name: "updateTokenSalePrice",
-=======
-      {
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "send",
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
-<<<<<<< HEAD
     inputs: [],
     name: "withdrawAllTokens",
-=======
-    inputs: [
-      {
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "transferOwnership",
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
--- src/app/contract.ts
+++ src/app/contract.ts
@@ -5,13 +5,8 @@ import { client } from "./client";
 import { ICOABI } from "./abi";
 import { polygonAmoy } from "thirdweb/chains";
 
-<<<<<<< HEAD
 export const contractIco = getContract({
   address: Variables.IcoAddress,
-=======
-export const tokenContract = getContract({
-  address: Variables.TokenAddress,
->>>>>>> 512ce5079221205a7b3d65ee41868fec2a99da73
   chain: polygonAmoy,
   client: client,
   abi: ICOABI,
